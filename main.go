0e91d66af0492ba000e044d8260548e7d4f1bb2c
Rafael Acioly <aciolyr@gmail.com>
2018-08-21 09:34:12 -0300

    - add postmon and republica virtual - create single function to run goroutines

--- main.go
+++ main.go
@@ -1,48 +1,59 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
-type cep struct {
-	Cep          string `json:"cep"`
-	State        string `json:"state"`
-	City         string `json:"city"`
-	Street       string `json:"street"`
-	Neighborhood string `json:"neighborhood"`
-}
-
 var endpoints = map[string]string{
-	"correios":  "",
-	"viacep":    "viacep.com.br/ws/%s/json/",
-	"cepaberto": "",
+	"viacep":           "https://viacep.com.br/ws/%s/json/",
+	"postmon":          "https://api.postmon.com.br/v1/cep/%s",
+	"republicavirtual": "http://republicavirtual.com.br/web_cep.php?cep=%s&formato=json",
 }
 
 func main() {
-	tube := make(chan string, 3)
+	http.HandleFunc("/v1/", handler)
 
-	for _, url := range endpoints {
-		zip := fmt.Sprintf(url, "cep")
-		// TODO: realizar chamadas usando goroutines para cada metodo que fornece
-		// informações sobre o cep
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
 	}
 
-	response := <-tube
-	var information cep
-	content := json.Unmarshal(response, &information)
-
-	json.NewEncoder(w).Encode(information)
+	fmt.Println("Listening on port: " + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func fromCorreios(zip string, tube chan []byte) {
-	// TODO: implementar requisição para os correios
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	tube := make(chan []byte, 3)
+	for source, url := range endpoints {
+		endpoint := fmt.Sprintf(url, r.URL.Path[1:])
+		go request(endpoint, source, tube)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%v", string(<-tube))
 }
 
-func fromViaCep(zip string, tube chan []byte) {
-	// TODO: implementar requisiçãoo para o viacep
-}
+func request(endpoint, source string, tube chan []byte) {
+	request, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Printf("Could not get from %s - %s \n", source, err.Error())
+	}
+	defer request.Body.Close()
 
-func fromCepAberto(zip string, tube chan []byte) {
-	// TODO: implementar requisição para cep aberto
+	response, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		fmt.Printf("Could not get payload from %s - %s", source, err.Error())
+	}
+
+	fmt.Printf("endpoint %s won \n", source)
+	tube <- response
 }
